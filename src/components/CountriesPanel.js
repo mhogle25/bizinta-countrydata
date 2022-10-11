1fe7fd52c3ca0f9f849c5d98642d76ab976216f3
Mitchell Hogle <mhogle@ehuman.com>
2022-10-11 13:13:35 -0700

    cleaned up the code a bit

--- src/components/CountriesPanel.js
+++ src/components/CountriesPanel.js
@@ -6,6 +6,7 @@ import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "@blueprintjs/table/lib/css/table.css";
 import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
 
+//Queries for a list of countries based on the continent code passed
 const COUNTRIES_BY_CONTINENT_QUERY = gql`
   query GetCountriesFromContinent($filterInput: CountryFilterInput!) {
       countries(filter: $filterInput) {
@@ -13,14 +14,11 @@ const COUNTRIES_BY_CONTINENT_QUERY = gql`
           name
           emoji
           capital
-          continent {
-              code
-              name
-          }
       }
   }
 `
 
+//Queries for all countries in the DB
 const COUNTRIES_QUERY = gql`
   query GetAllCountries {
       countries {
@@ -28,54 +26,43 @@ const COUNTRIES_QUERY = gql`
           name
           emoji
           capital
-          continent {
-              code
-              name
-          }
       }
   }
 `
 
+//Visualization of the table loading data
+const renderTableContent = (data, loading) => {
+  if (loading) return (
+    <tr>
+      <th>Loading...</th>
+      <th>Loading...</th>
+      <th>Loading...</th>
+    </tr>
+  )
+
+  return (
+    data && data.countries.map((country) => {
+      return (
+        <tr key={country.code}>
+          <td>{country.emoji}</td>
+          <td>{country.name}</td>
+          <td>{country.capital}</td>
+        </tr>
+      )
+    })
+  )
+}
+
 const CountriesPanel = ({id}) => {
+  //If this panel is a child of the World tab, query all countries
   let query = COUNTRIES_BY_CONTINENT_QUERY;
   if (id === "WO") {
     query = COUNTRIES_QUERY;
   }
 
-  const { data, loading, error }= useQuery(query,
-    {
-      variables: {
-        filterInput: {
-          continent: {
-            eq: id
-          }
-        }
-      }
-    });
+  const { data, loading, error }= useQuery(query, { variables: { filterInput: { continent: { eq: id } } } });
 
   if (error) console.log(error);
-
-  const renderTableContent = () => {
-    if (loading) return (
-      <tr>
-        <th>Loading...</th>
-        <th>Loading...</th>
-        <th>Loading...</th>
-      </tr>
-    )
-
-    return (
-      data && data.countries.map((country) => {
-        return (
-          <tr key={country.code}>
-            <td>{country.emoji}</td>
-            <td>{country.name}</td>
-            <td>{country.capital}</td>
-          </tr>
-        )
-      })
-    )
-  }
 
   return (
     <div className="CountriesPanel">
@@ -88,7 +75,7 @@ const CountriesPanel = ({id}) => {
           </tr>
         </thead>
         <tbody>
-        {renderTableContent()}
+        {renderTableContent(data, loading)}
         </tbody>
       </table>
     </div>
