dd667c6e828992f42655da199f0169c14980af30
Mitchell Hogle <mhogle@ehuman.com>
2022-10-20 12:19:52 -0700

    removed unneccessary comments

--- src/index.js
+++ src/index.js
@@ -2,22 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-//import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-//import ErrorPage from "./error-page";
-
-/*
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    errorElement: <ErrorPage/>
-  },
-])
-*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<RouterProvider router={ router }/>
   <App/>
 );
 
