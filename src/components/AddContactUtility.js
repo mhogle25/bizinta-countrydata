6666290d3abbb716d55d7b2ad79895815fcea9c8
Mitja Slenc <mslenc@gmail.com>
2022-11-11 02:43:26 +0100

    Minor AddContactUtility clean-up
    * editing is a better name than edit
    * Button instead of button
    * no point in wrapping the button rendering inside a function -
      you're just creating the function even when you don't need it,
      and with it now being a one-liner, there's hardly any point

--- src/components/AddContactUtility.js
+++ src/components/AddContactUtility.js
@@ -1,40 +1,31 @@
-import { Icon } from "@blueprintjs/core";
+import React from 'react';
+import { Button } from "@blueprintjs/core";
 import { useState } from "react";
 import UpdateContactUtility from "./UpdateContactUtility";
 
 const AddContactUtility = ({ contacts, setContacts }) => {
-  const [ edit, setEdit ] = useState(false);
-
-  const renderAddButton = () => {
-    return(
-      <button
-        className="bp4-button bp4-fill"
-        onClick={() => {
-          setEdit(!edit);
-        }}
-      >
-        <center><Icon icon="add"/></center>
-      </button>
-    )
-  }
+  const [ editing, setEditing ] = useState(false);
 
   return (
-    <div>
-      {edit ?
-        <UpdateContactUtility
-          confirmMessage="Add Contact"
-          onConfirm={(newContact) => {
-            setContacts([...contacts, newContact]);
-            setEdit(!edit);
-          }}
-          onCancel={() => {
-            setEdit(!edit);
-          }}
-        /> :
-        renderAddButton()
+    <React.Fragment>
+      { !editing &&
+      <Button fill icon="add" onClick={ () => setEditing(true) }/>
       }
-    </div>
-  )
+
+      { editing &&
+      <UpdateContactUtility
+        confirmMessage="Add Contact"
+        onConfirm={(newContact) => {
+          setContacts([...contacts, newContact]);
+          setEditing(false);
+        }}
+        onCancel={() => {
+          setEditing(false);
+        }}
+      />
+      }
+    </React.Fragment>
+  );
 }
 
 export default AddContactUtility;
