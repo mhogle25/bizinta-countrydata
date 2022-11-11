3bfaf98074fb911866a08c4510d75cee8cf52cdf
Mitja Slenc <mslenc@gmail.com>
2022-11-11 03:42:27 +0100

    Removed some redundancy in General Info
    When you see something repeating more than 2-3 times, it's probably
    worth it to remove the redundancy, especially when it's as easy as here.
    Compare readability between the two..
    
    Also, creating a function like this renderTable(), which simply renders
    a table, and is called once, almost immediately after being declared,
    kinda doesn't serve any purpose, it just adds noise.
    
    Also, createRow didn't have to be inside the component (getting
    recreated all the time), so it was moved out.

--- src/components/CountryInfoGeneralPanel.js
+++ src/components/CountryInfoGeneralPanel.js
@@ -1,49 +1,36 @@
 import EditableCommentCard from "./EditableCommentCard";
 import EditableURLCard from "./EditableURLCard";
+import { HTMLTable } from "@blueprintjs/core";
 
-const CountryInfoGeneralPanel = ({ country }) => {
-  const createRow = (header, value) => {
-    return (
-      <tr>
-        <td>{header}</td>
-        <td>{value}</td>
-      </tr>
-    )
-  }
-
-  //Conditionally render all rows based on whether the field exists in the country data
-  const renderTable = () => {
-    return (
-      <div>
-        <table
-          className="bp4-html-table bp4-html-table-striped"
-          style={{width:"100%", marginLeft:"auto", marginRight:"auto"}}
-        >
-          <tbody>
-          {country.emoji ? createRow("Flag", country.emoji) : null}
-          {country.name ? createRow("Name", country.name) : null}
-          {country.native ? createRow("Native Spelling", country.native) : null}
-          {country.phone ? createRow("Phone Extension", country.phone) : null}
-          {(country.continent !== null) && (country.continent.name !== null) ? createRow("Continent", country.continent.name) : null}
-          {country.capital ? createRow("Capital", country.capital) : null}
-          {country.currency ? createRow("Currency", country.currency) : null}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+function createRow(header, value) {
+  if (!value)
+    return null;
 
   return (
-    <div className="CountryInfoGeneralPanel">
-      { renderTable() }
-      <EditableCommentCard
-        countryCode={ country.code }
-      />
-      <EditableURLCard
-        countryCode={ country.code }
-      />
-    </div>
+    <tr>
+      <td>{ header }</td>
+      <td>{ value }</td>
+    </tr>
   );
 }
+
+const CountryInfoGeneralPanel = ({ country }) => (
+  <div className="CountryInfoGeneralPanel">
+    <HTMLTable striped style={{ width: '100%' }}>
+      <tbody>
+        { createRow("Flag",            country.emoji) }
+        { createRow("Name",            country.name) }
+        { createRow("Native Spelling", country.native) }
+        { createRow("Phone Extension", country.phone) }
+        { createRow("Continent",       country.continent?.name) }
+        { createRow("Capital",         country.capital) }
+        { createRow("Currency",        country.currency) }
+      </tbody>
+    </HTMLTable>
+
+    <EditableCommentCard countryCode={ country.code }/>
+    <EditableURLCard countryCode={ country.code }/>
+  </div>
+);
 
 export default CountryInfoGeneralPanel;
